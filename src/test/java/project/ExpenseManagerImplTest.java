1d6cdf6a897236b3350a7e07390455090924acfe
levejhak <ondrej.gruza@gmail.com>
2015-05-28 13:46:50 +0200

    ExpenseTests

--- src/test/java/project/ExpenseManagerImplTest.java
+++ src/test/java/project/ExpenseManagerImplTest.java
@@ -155,7 +155,7 @@ public class ExpenseManagerImplTest {
     }
 
     /**
-     * Test of getAllPayments method, of class ExpenseManagerImpl.
+     * Test of getAllPaymentsByAccount method, of class ExpenseManagerImpl.
      */
     @Test
     public void testGetAllPaymentsByAccount_3args() {
@@ -192,39 +192,184 @@ public class ExpenseManagerImplTest {
     }
 
     /**
+     * Test of getAllPaymentsBySubjectAndAccount method, of class ExpenseManagerImpl.
+     */
+   @Test
+    public void testGetAllPaymentsBySubjectAndAccount_Long_Long() {
+        System.out.println("getAllPaymentsBySubjectAndAccount");
+        
+        Long accountId1 = new Long(5);
+        Long accountId2 = new Long(4);
+        Long subjectId1 = new Long(6);
+        Long subjectId2 = new Long(9);
+        Long categoryId = new Long(7);
+        
+        BigDecimal price1 = new BigDecimal(130.00);
+        Payment payment1 = newPayment("Running shoes Nike",date("2015-05-20"),price1,accountId1,subjectId1,categoryId);
+        paymentManager.createPayment(payment1);
+        
+        BigDecimal price2 = new BigDecimal(100.00);
+        Payment payment2 = newPayment("Running shoes Adidas",date("2015-05-21"),price2,accountId1,subjectId2,categoryId);
+        paymentManager.createPayment(payment2);
+        
+        BigDecimal price3 = new BigDecimal(1000.00);
+        Payment payment3 = newPayment("Running shoes Puma",date("2015-05-14"),price3,accountId2,subjectId1,categoryId);
+        paymentManager.createPayment(payment3); 
+        
+        BigDecimal price4 = new BigDecimal(500.00);
+        Payment payment4 = newPayment("T-shirt Puma",date("2015-05-14"),price4,accountId2,subjectId2,categoryId);
+        paymentManager.createPayment(payment4);  
+        
+        BigDecimal price5 = new BigDecimal(2000.00);
+        Payment payment5 = newPayment("T-shirt Nike",date("2015-05-19"),price5,accountId1,subjectId2,categoryId);
+        paymentManager.createPayment(payment5);  
+        
+        List<Payment> expResult = new ArrayList<>();
+        expResult.add(payment2);
+        expResult.add(payment5);
+        List<Payment> result = expenseManager.getAllPaymentsBySubjectAndAccount(accountId1, subjectId2);
+        assertEquals(expResult.size(), result.size());
+        assertDeepEquals(expResult, result);
+        
+        paymentManager.deletePayment(payment1.getId());
+        paymentManager.deletePayment(payment2.getId());
+        paymentManager.deletePayment(payment3.getId());
+        paymentManager.deletePayment(payment4.getId());
+        paymentManager.deletePayment(payment5.getId());
+        
+    }
+
+    /**
+     * Test of getAllPaymentsBySubjectAndAccount method, of class ExpenseManagerImpl.
+     */
+    @Test
+    public void testGetAllPaymentsBySubjectAndAccount_4args() {
+        System.out.println("getAllPaymentsBySubjectAndAccountDates");
+        
+        Long accountId1 = new Long(5);
+        Long accountId2 = new Long(4);
+        Long subjectId1 = new Long(6);
+        Long subjectId2 = new Long(9);
+        Long categoryId = new Long(7);
+        Date startDate = date("2015-05-15");
+        Date endDate = date("2015-05-25");
+        
+        BigDecimal price1 = new BigDecimal(130.00);
+        Payment payment1 = newPayment("Running shoes Nike",date("2015-05-20"),price1,accountId1,subjectId1,categoryId);
+        paymentManager.createPayment(payment1);
+        
+        BigDecimal price2 = new BigDecimal(100.00);
+        Payment payment2 = newPayment("Running shoes Adidas",date("2015-05-21"),price2,accountId1,subjectId2,categoryId);
+        paymentManager.createPayment(payment2);
+        
+        BigDecimal price3 = new BigDecimal(1000.00);
+        Payment payment3 = newPayment("Running shoes Puma",date("2015-05-14"),price3,accountId2,subjectId1,categoryId);
+        paymentManager.createPayment(payment3); 
+        
+        BigDecimal price4 = new BigDecimal(500.00);
+        Payment payment4 = newPayment("T-shirt Puma",date("2015-05-14"),price4,accountId1,subjectId2,categoryId);
+        paymentManager.createPayment(payment4);  
+        
+        BigDecimal price5 = new BigDecimal(2000.00);
+        Payment payment5 = newPayment("T-shirt Nike",date("2015-05-19"),price5,accountId1,subjectId2,categoryId);
+        paymentManager.createPayment(payment5);  
+        
+        List<Payment> expResult = new ArrayList<>();
+        expResult.add(payment2);
+        expResult.add(payment5);
+        List<Payment> result = expenseManager.getAllPaymentsBySubjectAndAccount(accountId1, subjectId2, startDate, endDate);
+        assertEquals(expResult.size(), result.size());
+        assertDeepEquals(expResult, result);
+        
+        paymentManager.deletePayment(payment1.getId());
+        paymentManager.deletePayment(payment2.getId());
+        paymentManager.deletePayment(payment3.getId());
+        paymentManager.deletePayment(payment4.getId());
+        paymentManager.deletePayment(payment5.getId());
+    }
+    
+    /**
      * Test of getAllPaymentsBySubject method, of class ExpenseManagerImpl.
      */
-   /* @Test
-    public void testGetAllPaymentsBySubject_Long_Long() {
+    @Test
+    public void testGetAllPaymentsBySubject_Long() {
         System.out.println("getAllPaymentsBySubject");
-        Long accountId = null;
-        Long subjectId = null;
-        ExpenseManagerImpl instance = new ExpenseManagerImpl();
-        List<Account> expResult = null;
-        List<Account> result = instance.getAllPaymentsBySubject(accountId, subjectId);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }*/
+        
+        Long accountId1 = new Long(5);        
+        Long subjectId1 = new Long(6);
+        Long subjectId2 = new Long(9);
+        Long categoryId = new Long(7);   
+        Date startDate = date("2015-05-15");
+        Date endDate = date("2015-05-25");
+        
+        BigDecimal price1 = new BigDecimal(130.00);
+        Payment payment1 = newPayment("Running shoes Nike",date("2015-05-20"),price1,accountId1,subjectId1,categoryId);
+        paymentManager.createPayment(payment1);
+        
+        BigDecimal price2 = new BigDecimal(100.00);
+        Payment payment2 = newPayment("Running shoes Adidas",date("2015-05-21"),price2,accountId1,subjectId2,categoryId);
+        paymentManager.createPayment(payment2);
+        
+        BigDecimal price3 = new BigDecimal(1000.00);
+        Payment payment3 = newPayment("Running shoes Puma",date("2015-05-14"),price3,accountId1,subjectId1,categoryId);
+        paymentManager.createPayment(payment3); 
+        
+        BigDecimal price4 = new BigDecimal(500.00);
+        Payment payment4 = newPayment("T-shirt Puma",date("2015-05-14"),price4,accountId1,subjectId2,categoryId);
+        paymentManager.createPayment(payment4);          
+                
+        List<Payment> expResult = new ArrayList<>();
+        expResult.add(payment2);        
+        List<Payment> result = expenseManager.getAllPaymentsBySubject(subjectId2, startDate, endDate);
+        assertEquals(expResult.size(), result.size());
+        assertDeepEquals(expResult, result);
+        
+        paymentManager.deletePayment(payment1.getId());
+        paymentManager.deletePayment(payment2.getId());
+        paymentManager.deletePayment(payment3.getId());
+        paymentManager.deletePayment(payment4.getId());
+    }
 
     /**
      * Test of getAllPaymentsBySubject method, of class ExpenseManagerImpl.
      */
-    /*@Test
-    public void testGetAllPaymentsBySubject_4args() {
-        System.out.println("getAllPaymentsBySubject");
-        Long accountId = null;
-        Long subjectId = null;
-        Date startDate = null;
-        Date endDate = null;
-        ExpenseManagerImpl instance = new ExpenseManagerImpl();
-        List<Account> expResult = null;
-        List<Account> result = instance.getAllPaymentsBySubject(accountId, subjectId, startDate, endDate);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }*/
-
+    @Test
+    public void testGetAllPaymentsBySubject_3args() {
+        System.out.println("getAllPaymentsBySubjectDates");
+        
+        Long accountId1 = new Long(5);        
+        Long subjectId1 = new Long(6);
+        Long subjectId2 = new Long(9);
+        Long categoryId = new Long(7);        
+        
+        BigDecimal price1 = new BigDecimal(130.00);
+        Payment payment1 = newPayment("Running shoes Nike",date("2015-05-20"),price1,accountId1,subjectId1,categoryId);
+        paymentManager.createPayment(payment1);
+        
+        BigDecimal price2 = new BigDecimal(100.00);
+        Payment payment2 = newPayment("Running shoes Adidas",date("2015-05-21"),price2,accountId1,subjectId2,categoryId);
+        paymentManager.createPayment(payment2);
+        
+        BigDecimal price3 = new BigDecimal(1000.00);
+        Payment payment3 = newPayment("Running shoes Puma",date("2015-05-14"),price3,accountId1,subjectId1,categoryId);
+        paymentManager.createPayment(payment3); 
+        
+        BigDecimal price4 = new BigDecimal(500.00);
+        Payment payment4 = newPayment("T-shirt Puma",date("2015-05-14"),price4,accountId1,subjectId2,categoryId);
+        paymentManager.createPayment(payment4);          
+                
+        List<Payment> expResult = new ArrayList<>();
+        expResult.add(payment2);
+        expResult.add(payment4);
+        List<Payment> result = expenseManager.getAllPaymentsBySubject(subjectId2);
+        assertEquals(expResult.size(), result.size());
+        assertDeepEquals(expResult, result);
+        
+        paymentManager.deletePayment(payment1.getId());
+        paymentManager.deletePayment(payment2.getId());
+        paymentManager.deletePayment(payment3.getId());
+        paymentManager.deletePayment(payment4.getId());
+    }
        
     private static Payment newPayment(String description, Date date, BigDecimal amount, Long accountId, Long subjectId, Long categoryId) {
         Payment payment = new Payment();
