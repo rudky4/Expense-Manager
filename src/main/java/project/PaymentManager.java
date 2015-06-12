269ae3fde5bf688dac8cec538d171c27b09f8588
charlliz <andrea.turiakova@gmail.com>
2015-06-12 23:46:52 +0200

    doc

--- src/main/java/project/PaymentManager.java
+++ src/main/java/project/PaymentManager.java
@@ -15,15 +15,42 @@ import java.util.List;
  */
 public interface PaymentManager {
 
+    /**
+     * Add ceartin payment to database
+     * @param payment  payment to add to database
+     */
     public void createPayment(Payment payment);
 
+    /**
+     * Edit payment
+     * @param payment payment in database to be updated
+     */
     public void updatePayment(Payment payment);
 
+    /**
+     * Delete payment in database
+     * @param id id of payment
+     */
     public void deletePayment(Long id);
 
+    /**
+     * Return payment by id
+     * @param id payment id
+     * @return return payment 
+     */
     public Payment getPaymentById(Long id);
     
+    /**
+     * Return all payments in database
+     * @return list od payments
+     */
     public List<Payment> findAllPayments();
     
+    /**
+     * Return all payments in time interval
+     * @param startDate from date
+     * @param endDate to date
+     * @return list of payments
+     */
     public List<Payment>findAllPayments(Date startDate, Date endDate);
 }
--- src/main/java/project/SubjectManager.java
+++ src/main/java/project/SubjectManager.java
@@ -15,15 +15,41 @@ import java.util.List;
  */
 public interface SubjectManager {
 
+    /**
+     * Add certain subject to database
+     * @param subject subject to be added
+     */
     public void createSubject(Subject subject);
 
+    /**
+     * Edit certain subject in database
+     * @param subject subject to be edited
+     */
     public void updateSubject(Subject subject);
 
+    /**
+     * Delete subject in database
+     * @param id id of subject
+     */
     public void deleteSubject(Long id);
 
+    /**
+     * Return certain subject by id
+     * @param id subject id
+     * @return subject
+     */
     public Subject getSubjectById(Long id);
     
+    /**
+     * Return certain subject by name
+     * @param name name of subject
+     * @return subject
+     */
     public Subject getSubjectByName(String name);
     
+    /**
+     * Return all subjects in database
+     * @return list of subjects
+     */
     public List<Subject> findAllSubjects();
 }
